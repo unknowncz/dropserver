7fd60700e00774cd89bf491cd921d2bf886fec53
unknowncz <86740586+unknowncz@users.noreply.github.com>
2022-02-18 19:36:04 +0100

    Functional

--- main.js
+++ main.js
@@ -3,27 +3,42 @@ swch = document.getElementsByClassName('switch');
 upload_view = document.getElementsByClassName('upload-view')[0];
 file_sys_view = document.getElementsByClassName('file-sys-view')[0];
 svg = document.getElementsByTagName('svg');
+folders = document.getElementsByClassName('folder')
+
+const emptydiv = document.createElement('div')
+emptydiv.textContent = '-Empty-'
+emptydiv.className = 'file'
+
+const fulldiv = document.createElement('div')
+const a = document.createElement('a');
+
+function insertAfter(newNode, existingNode) {
+    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+}
 
 document.addEventListener("dragover", (e) => {
     e.preventDefault();
 });
 
 document.addEventListener('dragstart', () => {
-    body.setAttribute('style', 'background-color: rgba(248, 247, 216, 0.7);')
+    if (!upload_view.hasAttribute('style')) {
+        body.setAttribute('style', 'background-color: rgba(248, 247, 216, 0.7);')
+    }
 });
 
 document.addEventListener('drop', (e) => {
-    console.log('gootis');
-    e.preventDefault();
-    if(e.dataTransfer.items){
-        for(i=0;i<e.dataTransfer.files.length;i++){
-            if (e.dataTransfer.items[i].kind === 'file') {
-                let req = new XMLHttpRequest();
-                let formData = new FormData();
+    if (!upload_view.hasAttribute('style')) {
+        e.preventDefault();
+        if(e.dataTransfer.items){
+            for(i=0;i<e.dataTransfer.files.length;i++){
+                if (e.dataTransfer.items[i].kind === 'file') {
+                    let req = new XMLHttpRequest();
+                    let formData = new FormData();
 
-                formData.append("content", e.dataTransfer.items[i].getAsFile());                                
-                req.open("POST", '/upload');
-                req.send(formData);
+                    formData.append("content", e.dataTransfer.items[i].getAsFile());                                
+                    req.open("POST", '/upload');
+                    req.send(formData);
+                }
             }
         }
     }
@@ -34,13 +49,12 @@ document.addEventListener('dragend', () => {
 });
 
 let swfn = () => {
-    if(upload_view.hasAttribute('style')){
+    if (upload_view.hasAttribute('style')){
         upload_view.removeAttribute('style');
         file_sys_view.setAttribute('style', 'display: none;');
         svg[0].removeAttribute('style');
         //document.head.innerHTML = document.head.innerHTML.replace('<link rel="stylesheet" href="/fsys.css">', '<!--<link rel="stylesheet" href="/fsys.css">-->')
-    }
-    else {
+    } else {
         svg[0].setAttribute('style', 'display: none;');
         file_sys_view.removeAttribute('style');
         upload_view.setAttribute('style', 'display: none;');
@@ -51,3 +65,40 @@ let swfn = () => {
 swch[0].addEventListener('click', swfn);
 swch[1].addEventListener('click', swfn);
 
+for (i=0;i<folders.length;i++) {
+    folders[i].addEventListener('click', (e) => {
+        x = new XMLHttpRequest();
+        x.open("GET", "/getfiles" + e.target.innerText.trim(), false);
+        x.send();
+        if (x.status == 200) {
+            emptydiv.hidden = true
+            fulldiv.hidden = true
+            fulldiv.innerHTML = ''
+            if (x.response.length > 2) {
+                console.log(x.response.split('\r').join('').split('<>'))
+                for(j=0;j<x.response.split('\r').join('').split('<>').length;j++){
+                    f = x.response.split('\r').join('').split('<>')[j]
+                    fulldiv.innerHTML += '<button class=\'file\' onclick=\'reqfile("' + f + '")\'><span class=\'colorfull\' style=\'background-color: rgb(91, 123, 121);\'>&nbsp;&nbsp;</span>&nbsp;' + f + '</button><br>'
+                }
+                insertAfter(fulldiv, e.target.parentElement)
+                fulldiv.hidden = false
+            } else {
+                insertAfter(emptydiv, e.target.parentElement)
+                emptydiv.hidden = false
+            }
+        };
+    })
+}
+
+function reqfile(f) {
+    xhr = new XMLHttpRequest();
+    xhr.open('POST', "/getfile" + fulldiv.previousSibling.textContent.trim() + '/' + f, true);
+    xhr.responseType = 'blob';
+    xhr.onload = function (e) {
+        var blob = e.currentTarget.response;
+        a.href = window.URL.createObjectURL(blob);
+        a.download = f;
+        a.dispatchEvent(new MouseEvent('click'))
+    }
+    xhr.send();
+}
--- server.py
+++ server.py
@@ -1,8 +1,10 @@
+import os
 import socket
 import threading
 import email
 from io import StringIO
 import time
+import traceback
 
 
 class handler:
@@ -21,14 +23,16 @@ class handler:
             '/main.css':self.servefile,
             '/main.js':self.servefile,
             '/fsys.css':self.servefile,
-            '/upload':self.savefile
+            '/upload':self.savefile,
+            '/getfiles':self.foldercontents,
+            '/getfile':self.filedownload
         }
         self.rq = conn.recv(1024).decode()
         self.request, headers, *overflow = self.rq.split('\r\n', 1) + ['', '']
         message=email.message_from_file(StringIO(headers))
         self.h=dict(message.items())
         if self.request.split(' ')[1]!='/':
-            self.lookupTable[self.request.split(' ')[1]](self.request.split(' ')[1], self.h)
+            self.lookupTable.get('/' + self.request.split(' ')[1].split('/')[1], self.default())(self.request.split(' ')[1], self.h)
         else:
             self.servefile(self.request.split(' ')[1])
 
@@ -46,43 +50,77 @@ class handler:
         time.sleep(0.1)
         self.conn.close()
 
+    def default(self, *overflow):
+        pass
+
+    def filedownload(self, path, *overflow):
+        relative_directory = '/'.join(path.split('/')[2:]).replace('\\', '').replace('//', '')
+        with open(f'{__file__}\\..\\{relative_directory}', 'rb') as f:
+            self.setheaders([b'HTTP/1.1 200 OK',  b'Accept-Ranges:bytes',  b'Content-Length:' + str(os.path.getsize(f'{__file__}\\..\\{relative_directory}')).encode()])
+            self.endheaders()
+            self.write(b'\r\n'.join(f.readlines()))
+            self.flush()
+
+    def foldercontents(self, folderpath, *overflow):
+        relative_directory = '/'.join(folderpath.split('/')[2:]).replace('.', '').replace('//', '')
+        direcc = '<>'.join(os.listdir(f'{__file__}\\..\\{relative_directory}')) + '\r\n'
+        h = [b'HTTP/1.1 200 OK',  b'Accept-Ranges:bytes',  b'Content-Length:' + str(len(direcc)).encode()]
+        self.setheaders(h)
+        self.endheaders()
+        time.sleep(0.1)
+        self.write(direcc.encode())
+        self.flush()
+
     def savefile(self, _, h, *overflow):
-        b = self.h['Content-Type'].strip('multipart/form-data; boundary=')
-        temp = self.rq.split(f'--{b}')[2].split('\r\n', 3)
-        filename = temp[0].split('\r\n')[1].split('filename=')[1].strip('"')
-        print(self.rq)
-        with open(f'{__file__}\\..\\uploads\\{filename}', 'x')as f:
-            try: 
-                f.write(temp[1])
-                x = len(temp[1])
-            except:
-                x = 0
-            while int(self.h['Content-Length']) > x:
-                f.write(conn.recv(2048).decode().strip().strip(f'--{b}'))
-                x += 2048
+        try:
+            b = self.h['Content-Type'].strip('multipart/form-data; boundary=')
+            temp = self.rq.split(f'--{b}')[2].split('\r\n', 3)
+            for i in temp:
+                try:
+                    filename = i.split('filename=')[1].strip('"')
+                except:
+                    pass
+            with open(f'{__file__}\\..\\uploads\\{filename}', 'x')as f:
+                try: 
+                    f.write(temp[-1].split('\r\n')[1])
+                    x = len(temp[-1].split('\r\n')[1])
+                except:
+                    x = 0
+                while int(self.h['Content-Length']) > x:
+                    if int(self.h['Content-Length']) > x:break
+                    f.write(conn.recv(2048).decode().strip().strip(f'--{b}'))
+                    x += 2048
+                self.setheaders([b'HTTP/1.1 200 OK',  b'Accept-Ranges:bytes', b'Connection:close'])
+                self.endheaders()
+                self.write(b' ')
+                self.flush()
+        except Exception:
+            print(traceback.format_exc())
 
     def servefile(self, filename:str, *overflow):
-        try:ftype=self.filetypes[filename.split('.')[-1]]
-        except:ftype=b'text/html'
         try:
-            if filename=='/':
+            ftype = self.filetypes[filename.split('.')[-1]]
+        except:
+            ftype = b'text/html'
+        try:
+            if filename == '/':
                 with open(f'{__file__}\\..\\main.html', 'rb')as f:
-                    x=f.readlines()
-                    h=[b'HTTP/1.1 200 OK', b'Content-type:'+ftype, b'Connection:close', b'Content-Length:'+str(len(b''.join(x))).encode(), b'Accept-Ranges:bytes']
+                    x = f.readlines()
+                    h = [b'HTTP/1.1 200 OK', b'Content-type:' + ftype, b'Connection:close', b'Content-Length:' + str(len(b''.join(x))).encode(), b'Accept-Ranges:bytes']
                     self.setheaders(h)
                     self.endheaders()
                     time.sleep(0.1)
                     self.write(b''.join(x))
             else:
                 with open(f'{__file__}\\..{filename}', 'rb')as f:
-                    x=f.readlines()
-                    h=[b'HTTP/1.1 200 OK', b'Content-type:'+ftype, b'Connection:close', b'Content-Length:'+str(len(b''.join(x))).encode(), b'Accept-Ranges:bytes']
+                    x = f.readlines()
+                    h = [b'HTTP/1.1 200 OK', b'Content-type:' + ftype, b'Connection:close', b'Content-Length:' + str(len(b''.join(x))).encode(), b'Accept-Ranges:bytes']
                     self.setheaders(h)
                     self.endheaders()
                     time.sleep(0.1)
                     self.write(b''.join(x))
         except FileNotFoundError:
-            h=[b'HTTP/1.1 404 NOT FOUND', b'Connection:close', b'Content-type:'+ftype, b'Accept-Ranges:bytes']
+            h = [b'HTTP/1.1 404 NOT FOUND', b'Connection:close', b'Content-type:' + ftype, b'Accept-Ranges:bytes']
             self.setheaders(h)
             self.endheaders()
             time.sleep(0.1)
@@ -104,5 +142,5 @@ if __name__ == '__main__':
                 with conn:
                     time.sleep(0.2)
                     threading.Thread(target=handler, args=(conn,)).run()
-            except ConnectionAbortedError:
-                pass
+            except ConnectionAbortedError as e:
+                print(e.strerror)
